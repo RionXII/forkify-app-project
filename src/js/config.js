76081a9533d4dc71eb30d86e54b4938069892a48
RionXII <ryangimm@gmail.com>
2023-01-13 11:48:33 -0800

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const API_KEY = '15b2ae64-3dbc-4a2d-9151-359c25c534bf';
+export const TIMEOUT_SEC = 10;
+export const RESULTS_PER_PAGE = 10;
+export const MODAL_CLOSE_SECONDS = 2.5;
